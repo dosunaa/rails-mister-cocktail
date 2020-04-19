52f8ec87ed6f5013f1b08c28eb31c0a5d6a12221
David Osuna Azcona Dominguez <david.oad@gmail.com>
2020-04-18 18:49:41 -0600

    attributes, validations and associations

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  # validates [:cocktail_id, :ingredient_id], uniqueness: true
+  validates_uniqueness_of :cocktail_id, :scope => :ingredient_id
+end
