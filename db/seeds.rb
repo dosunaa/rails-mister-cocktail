677eaf5d11b810fa8a06d85eca130892a4e57fd0
David Osuna Azcona Dominguez <david.oad@gmail.com>
2020-04-25 02:16:10 -0600

    advances in front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+# Cocktail.create(name: "Mojito")
+# Cocktail.create(name: "Margarita")
+# Cocktail.create(name: "Moroccan Tea")
+
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_raw = open(url).read
+ingredients_json = JSON.parse(ingredients_raw)
+ingredients_json["drinks"].each do |ingredient|
+  Ingredient.create!(name: "#{ingredient["strIngredient1"]}")
+end
+
+#  create!(name: “#{ingredient[“strIngredient1”]}“)
+#  ingredients_json.("drinks").each do |ingredient|
